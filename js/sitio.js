4d85a4dc39c8fc562e5bca939a4993598a0ba624
Armandocs16 <ep-armand@hotmail.com>
2017-10-25 14:13:58 -0500

    Se agregaron animaciones a la seccion experiencias, y conocimientos

--- js/sitio.js
+++ js/sitio.js
@@ -3,25 +3,23 @@ new WOW().init();
 
 //inicializa  smooth scroll
 //
-var scroll = new SmoothScroll('a[href*="#"]', {
+/*var scroll = new SmoothScroll('a[href*="#"]', {
 	
 	// Speed & Easing
-	speed: 1000, // Integer. How fast to complete the scroll in milliseconds
-	offset: 80, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+	speed: 6000, // Integer. How fast to complete the scroll in milliseconds
+	offset: 6000, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
 	easing: 'easeInOutCubic', // Easing pattern to use
 	customEasing: function (time) {}, // Function. Custom easing pattern
+});*/
+
+/*$("document").ready(function($){
+
+$(window).width(function () {
+    if ($(window).width() <= 480) {
+    	$('#seccion').addClass('wow fadeInUp');
+	}
+ 	else {
+    	$('#seccion').removeClass('wow fadeInUp');
+	}
 });
-
-$(function () {
-	$(window).scroll(function () {
-		var scrolltop = $(this).scrollTop();
-		if(scrolltop >= 50){
-			$(".id-arriba").fadeIn();
-		}
-		else{
-			$(".id-arriba").fadeOut();
-		}
-	});
-});
-
-
+});*/
